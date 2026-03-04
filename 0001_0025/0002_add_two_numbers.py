491ea9b0e383acadf0c392d64f7a80d0712a6aff
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-04 18:01:54 +0800

    solve 0002 add two numbers -1

---
+++ 0001_0025/0002_add_two_numbers.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        dummy = ListNode(0)
+        carry = 0 
+        p = dummy
+        while l1 and l2:
+            p.next = ListNode((l1.val + l2.val + carry)%10)
+            carry = (l1.val + l2.val + carry)//10
+            p = p.next
+            l1 = l1.next
+            l2 = l2.next
+        if l2:
+            while l2:
+                p.next = ListNode((l2.val + carry)%10)
+                carry = (l2.val + carry)//10
+                p = p.next
+                l2 = l2.next
+        if l1:
+            while l1:
+                p.next = ListNode((l1.val + carry)%10)
+                carry = (l1.val + carry)//10
+                p = p.next
+                l1 = l1.next
+        if carry != 0:
+            p.next = ListNode(1)
+        return dummy.next
