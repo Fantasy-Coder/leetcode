e42052bac5859f5cd33ae656ba2275a139f0ad0e
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-13 21:54:19 +0800

    move_zeroes -1 two_pointers

---
+++ hot_100/two-pointers/move_zeroes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        l = r = 0
+        while r < n:
+            if nums[r] != 0:
+                nums[l],nums[r] = nums[r],nums[l]
+                l += 1
+            r += 1
+        return nums
+    
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        j = 0 
+        if not nums:
+            return 0
+        for i in range(n):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+            i += 1
+        for i in range(j,n):
+            nums[i] = 0
+        return nums
