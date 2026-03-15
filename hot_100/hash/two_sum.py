ac89eb0882af78d06d608e26381ea5279599c899
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-15 20:44:45 +0800

    trapping_rain_water -1 two_pointers

--- hot_100/hash/two_sum.py
+++ hot_100/hash/two_sum.py
@@ -8,3 +8,12 @@ class Solution:
             temp_nums = nums[next:]
             if j in temp_nums:
                 return(nums.index(i),next + temp_nums.index(j))
+
+#hash
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i,num in enumerate(nums):
+            if target - num in hash:
+                return [ hash[target - num], i ]
+            hash[nums[i]] = i
