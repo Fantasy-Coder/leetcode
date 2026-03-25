a3bf0ceb05d9f04bd4037effc89dfb3040f97ffb
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-25 22:24:09 +0800

    rotate_image -1 matrix

---
+++ hot_100/matrix/rotate_image.py
@@ -0,0 +1,30 @@
+from typing import List
+import copy
+#辅助矩阵
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        temp = copy.deepcopy(matrix)
+        for i in range(n):
+            for j in range(n):
+                matrix[j][n - 1 - i] = temp[i][j]
+
+#原地旋转
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n // 2):
+            for j in range((n + 1) // 2):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = temp
+
+
