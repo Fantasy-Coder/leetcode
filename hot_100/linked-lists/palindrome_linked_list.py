b7575f54f553d9e23373e5b236fb173a204af852
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-27 23:58:57 +0800

    palindrome_linked_list -1 linked_lists

---
+++ hot_100/linked-lists/palindrome_linked_list.py
@@ -0,0 +1,47 @@
+from typing import Optional 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+#数组
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        current_node = head
+        while current_node is not None:
+            vals.append(current_node.val)
+            current_node = current_node.next
+        return vals == vals[::-1]
+ 
+#快慢指针找中点+反转后段链表+判断回文
+class Solution:
+    # 链表的中间结点
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    # 反转链表
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pre, cur = None, head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        mid = self.middleNode(head)
+        head2 = self.reverseList(mid)
+        while head2:
+            if head.val != head2.val:  # 不是回文链表
+                return False
+            head = head.next
+            head2 = head2.next
+        return True
+
