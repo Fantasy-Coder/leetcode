c0d1b360836f5ced1c1f4cc5da71b06212b5b691
Fantasy-Coder <wangjiangtao1@sxu.edu.cn>
2026-03-31 12:15:20 +0800

    swap_nodes_in_pairs -1 linked_lists

---
+++ hot_100/linked-lists/swap_nodes_in_pairs.py
@@ -0,0 +1,25 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+#迭代
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
+    
+#递归
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        newHead = head.next
+        head.next = self.swapPairs(newHead.next)
+        newHead.next = head
+        return newHead
